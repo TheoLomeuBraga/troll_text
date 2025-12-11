6e32cc6df009df2bf58089fbf6e799dfa1afe929
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2025-12-11 20:24:37 +0000

    undo some thing

--- src/troll_the_text.h
+++ src/troll_the_text.h
@@ -6,5 +6,4 @@ std::map<wchar_t,std::wstring> load_trool_alphabet(const std::string &path);
 
 std::wstring troll_the_char(std::map<wchar_t,std::wstring> &alphabet, const wchar_t &_char);
 
-
 std::wstring troll_the_text(std::map<wchar_t,std::wstring> &replacement, const std::wstring &text);
